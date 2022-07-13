89bb34e30959ade813d3bd89ad8b7a7a6ca666d0
DadriaS <109231598+DadriaS@users.noreply.github.com>
2022-07-13 10:57:58 -0500

    Update and rename aggregate water usage to water_usage.py

--- water_usage.py
+++ water_usage.py
@@ -1,3 +1,6 @@
+"""This script is done with the intention to read the water usage of multiple areas in 6 months which are recorded over 50 excel files and aggregate that
+reading in one place."""
+
 def  water_usage(filepath): #filepath should be in a raw string format
   import pandas as pd
   import os
@@ -14,9 +17,10 @@ def  water_usage(filepath): #filepath should be in a raw string format
     #formatting string variable as raw string
     file=r"{}".format(file)
     # reading excel files
-    read_data=pd.read_excel(file,"Production Output",usecols=["Day#","Date","Amount in Litres"])
+    read_data=pd.read_excel(file,"Production Output",usecols=["Date","Water Usage (L)"])
     df = pd.concat([df, read_data],verify_integrity=True,ignore_index=True)
-    
+  
+  #saving dataframe as a csv file  
   df.to_csv('Water Usage.csv',encoding='utf-8', index=False)
         
 water_usage(r"C:\Users\owner\Documents\Cycle 14")
